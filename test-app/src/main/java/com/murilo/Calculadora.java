4bf7b10fa35bfbe34228473730a420b63247fb8b
httpmurilo <muriloeduardo1997@outlook.com>
2023-08-09 18:33:54 -0300

    Continuação

--- test-app/src/main/java/com/murilo/Calculadora.java
+++ test-app/src/main/java/com/murilo/Calculadora.java
@@ -5,4 +5,12 @@ public class Calculadora {
     public int soma(int a, int b) {
         return a + b;
     }
+
+    public int divisao(int a, int b) {
+        return a / b;
+    }
+
+    public int subtracao(int a, int b) {
+        return a - b;
+    }
 }
--- test-app/src/test/java/com/murilo/CalculadoraTests.java
+++ test-app/src/test/java/com/murilo/CalculadoraTests.java
@@ -11,4 +11,27 @@ public class CalculadoraTests {
         var calculo = calculadora.soma(1,5);
         Assertions.assertEquals(6,calculo);
     }
+
+    @Test
+    public void deveRetornarZeroComNumeradorZeroNaDivisao() {
+        Calculadora calculadora = new Calculadora();
+        var calculo = calculadora.divisao(0,5);
+        Assertions.assertEquals(0,calculo);
+    }
+
+    @Test
+    public void deveRetornarErroDenominadorZeroNaDivisao() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            Calculadora calculadora = new Calculadora();
+            calculadora.divisao(5,0);
+
+        });
+    }
+
+    @Test
+    public void testSubtracao() {
+        Calculadora calculadora = new Calculadora();
+        var calculo = calculadora.subtracao(20,5);
+        Assertions.assertEquals(15,calculo);
+    }
 }
